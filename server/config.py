1646188f7040fb08d8661cc1a5b64d6048bb1bd0
Conch <8510@students.carboncti.org>
2024-01-18 17:01:50 +0000

    change a comment

--- server/config.py
+++ server/config.py
@@ -1,2 +1,3 @@
-COMMAND_PREFIX = "~"  # The prefix for commands. This is used to determine if a message is a command or not.
+# The prefix for commands. This is used to determine if a message is a command or not.
+COMMAND_PREFIX = "~"
 REQUIRED_USER_FIELDS = ["email", "username", "password", "dob"]
