03741ec361dc0ff92e3b62b1bcb305a2bcb9ad2b
whoisconch <conch@twisea.net>
2024-03-12 12:02:11 -0400

    Add interfaces and stuff to sveltekit

--- site/src/lib/index.ts
+++ site/src/lib/index.ts
@@ -1,1 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+
+export interface user {
+    username: string;
+    displayname: string;
+    permissions: Number;
+    creation_date: string;
+}
+
+export interface message { 
+    message: string;
+    author: user | string;
+    timestamp: string;
+    type: Number;
+    ephemeral: boolean;
+}
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,3 +1,30 @@
+<script lang="ts">
+    import { io } from "socket.io-client";
+    import type { message } from "$lib";
+
+    const socket = io("https://x36dw3tq-5000.use.devtunnels.ms/");
+
+    let messages: message[] = [
+        {
+            message: "Welcome to Retro Rendezvous!",
+            author: "Retro Rendezvous",
+            timestamp: "now",
+            type: 12,
+            ephemeral: false,
+        }
+    ];
+
+    socket.on("connect", () => {
+        console.log("Connected to server");
+    });
+
+    socket.on("message", (data: message) => {
+        console.log("Message from server", data);
+        messages = [...messages, data];
+    });
+</script>
+
+
 <h1>Retro Rendezvous!</h1>
 
 <head>
@@ -10,27 +37,10 @@
 </body>
 
 <div class="message">
-    {#each messages as message (message.id)}
-        <p>{message.text}</p>
+    {#each messages as message (message.message)}
+        <p>{message.message}</p>
     {/each}
 </div>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<script>
-    import { io } from "socket.io-client";
-
-    const socket = io("https://x36dw3tq-5000.use.devtunnels.ms/");
-
-    let messages = [];
-
-    socket.on("connect", () => {
-        console.log("Connected to server");
-    });
-
-    socket.on("message", (data) => {
-        console.log("Message from server", data);
-        messages = [...messages, data];
-    });
-</script>
