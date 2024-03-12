e215c49bc33fbf736793bee4a73862d9e1789fa8
whoisconch <conch@twisea.net>
2024-03-12 11:31:19 -0400

    Add some socketio stuff

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -10,7 +10,27 @@
 </body>
 
 <div class="message">
-
-    <!-- Messages will appear about here, lad! -->
+    {#each messages as message (message.id)}
+        <p>{message.text}</p>
+    {/each}
 </div>
 
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import { io } from "socket.io-client";
+
+    const socket = io("https://x36dw3tq-5000.use.devtunnels.ms/");
+
+    let messages = [];
+
+    socket.on("connect", () => {
+        console.log("Connected to server");
+    });
+
+    socket.on("message", (data) => {
+        console.log("Message from server", data);
+        messages = [...messages, data];
+    });
+</script>
