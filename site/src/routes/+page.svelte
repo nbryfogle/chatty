99129e7362b9a6a973819d138dc7927367895361
ZaneZimmermanCCTI <8545@students.carboncti.org>
2024-03-12 13:26:45 -0400

    Added a button for the svelte GUI we are starting to construct

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,25 +1,11 @@
-<h1>Retro Rendezvous!</h1>
-
-<head>
-    <title>Chat | App</title>
-</head>
-
-
-<body>
-    <h6><span>&copy;</span> Copyright, Norwegian Ball Waffles</h6>
-</body>
-
-<div class="message">
-    {#each messages as message (message.id)}
-        <p>{message.text}</p>
-    {/each}
-</div>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <script>
     import { io } from "socket.io-client";
+    import TestB from "../components/TestB.svelte";
+
+    function handleClick(){
+        alert('clicked')
+    }
 
     const socket = io("https://x36dw3tq-5000.use.devtunnels.ms/");
 
@@ -34,3 +20,23 @@
         messages = [...messages, data];
     });
 </script>
+
+
+<h1>Retro Rendezvous!</h1>
+
+
+
+
+<h6><span>&copy;</span> Copyright, Norwegian Ball Waffles</h6>
+    
+<TestB on:click={handleClick} />
+
+
+<div class="message">
+    {#each messages as message (message.id)}
+        <p>{message.text}</p>
+    {/each}
+</div>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
